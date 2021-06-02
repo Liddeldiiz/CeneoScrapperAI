f0e8ddb2ef29dc667ad2003d59ae0808b12d91f2
Liddeldiiz <s218711@student.uek.krakow.pl>
2021-06-02 13:18:10 +0200

    2021-06-02 ŁK

--- app/routes.py
+++ app/routes.py
@@ -72,31 +72,54 @@ def product(productBrand, productID):
         d = json.load(f)
     
     opinions = json_normalize(d['opinions'])
-    stars = opinions['stars']
+    stars = opinions['stars'].value_counts().sort_index(ascending=True).reindex(np.arange(0, 5.5, 0.5).tolist(), fill_value=0)
 
-    redomendations = opinions['recomendation'].value_counts(dropna=False)
-    averageScore = opinions['stars'].mean()
-    prosCount = opinions['advantages'].count()
-    consCount = opinions['disadvantages'].count()
-    purchased = opinions['purchaseDate'].count()
-    advantages = []
-    for a in opinions['advantages'].dropna().tolist():
-        advantages += a.split(', ')
-    pros = pd.Series(advantages, name="advantages").value_counts()
-    disadvantages = []
-    for a in opinions['disadvantages'].dropna().tolist():
-        disadvantages += a.split(', ')
-    cons = pd.Series(disadvantages, name="disadvantages").value_counts()
-    features = pros.to_frame().join(cons)
+    #redomendations = opinions['recomendation'].value_counts(dropna=False)
+    #averageScore = opinions['stars'].mean()
+    #prosCount = opinions['advantages'].count()
+    #consCount = opinions['disadvantages'].count()
+    #purchased = opinions['purchaseDate'].count()
+    #advantages = []
+    #for a in opinions['advantages'].dropna().tolist():
+    #    advantages += a.split(', ')
+    #pros = pd.Series(advantages, name="advantages").value_counts()
+    #disadvantages = []
+    #for a in opinions['disadvantages'].dropna().tolist():
+    #    disadvantages += a.split(', ')
+    #cons = pd.Series(disadvantages, name="disadvantages").value_counts()
+    #features = pros.to_frame().join(cons)
+
+    """data = [
+        
+        ("01-01-2020", 1597),
+        ("02-01-2020", 1456),
+        ("03-01-2020", 1908),
+        ("04-01-2020", 896),
+        ("05-01-2020", 755),
+        ("06-01-2020", 453),
+        ("07-01-2020", 1100),
+        ("08-01-2020", 1235),
+        ("09-01-2020", 1478),
+        
+    ]
+
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]"""
+
+    
+    labels = [row[0] for row in stars]
+    values = [row[1] for row in stars]
     
     return render_template('product.html.jinja', 
-    stars=stars, 
-    redomendations=redomendations, 
-    averageScore=averageScore,
-    prosCount=prosCount, 
-    consCount=consCount,
-    purchased=purchased,
-    features=features,
+    #stars=stars, 
+    #redomendations=redomendations, 
+    #averageScore=averageScore,
+    #prosCount=prosCount, 
+    #consCount=consCount,
+    #purchased=purchased,
+    #features=features,
+    labels=labels,
+    values=values,
     productBrand=productBrand, 
     productID=productID)
 
